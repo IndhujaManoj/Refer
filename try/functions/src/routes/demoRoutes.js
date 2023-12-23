8e5202378b2633d1ee89f3cf4a2561f0fda8901b
dRajkumard <rajkumar.d408@gmail.com>
2023-12-23 11:32:59 +0530

    refer

--- try/functions/src/routes/demoRoutes.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const demoController=require("../controllers/demoController");
-const router=express.Router();
-router.post("/create", demoController.createDemo);
-router.get("/:id", demoController.getDemoById);
-
-module.exports=router;
